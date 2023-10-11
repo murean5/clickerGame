9e43a2c6b6019467392563b44eb512460ff54f98
Anton Muravev <ayumuravev@edu.hse.ru>
2023-10-11 17:42:07 +0300

    Production improvements
    -added: store
    feat!: not it is possible to increase level of game resources production

--- Assets/_Source/Core/GameManager.cs
+++ Assets/_Source/Core/GameManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Gradient productionGradientColor;
+    public Transform footer;
     private void Awake()
     {
         ResourceBank.ChangeResource(GameResource.Humans, 10);
--- Assets/_Source/Core/ProductionBuilding.cs
+++ Assets/_Source/Core/ProductionBuilding.cs
@@ -9,18 +9,47 @@ public class ProductionBuilding : MonoBehaviour
 {
     [SerializeField] private GameResource currentGameResource;
     private const float BaseProductionTime = 1.1f;
+    private float _totalProductionTime;
     private Button _productionButton;
     private GameManager _gameManager;
     private Image _fillImage;
     private Slider _waitingSlider;
+    private Button _lvlUpButton;
+    private int _productAmount;
+
+    private Text _amountText;
 
     private void Awake()
     {
+        InitComponents();
+
+        _productionButton.onClick.AddListener(Product);
+        _lvlUpButton.onClick.AddListener(LevelUp);
+        _productAmount = 1;
+        CalculateTotalProductionTime();
+    }
+
+    private void InitComponents()
+    {
         if (Camera.main != null) _gameManager = Camera.main.GetComponent<GameManager>();
         _productionButton = GetComponent<Button>();
-        _productionButton.onClick.AddListener(Product);
+        _amountText = _productionButton.transform.GetComponentInChildren<Text>();
         _waitingSlider = _productionButton.transform.Find("WaitingSlider").GetComponent<Slider>();
         _fillImage = _waitingSlider.transform.Find("Fill Area/Fill").GetComponent<Image>();
+        _lvlUpButton = _gameManager.footer.Find($"StorePanel/LvlUp{currentGameResource}Button").GetComponent<Button>();
+    }
+
+    private void LevelUp()
+    {
+        _productAmount++;
+        _amountText.text = $"+{_productAmount}";
+        CalculateTotalProductionTime();
+    }
+
+    private void CalculateTotalProductionTime()
+    {
+        _totalProductionTime = BaseProductionTime + 0.1f * _productAmount;
+        _waitingSlider.maxValue = _totalProductionTime;
     }
 
     private void Product()
@@ -33,18 +62,17 @@ public class ProductionBuilding : MonoBehaviour
 
     private void Update()
     {
-        if (!_productionButton.interactable)
-        {
-            _waitingSlider.value -= Time.deltaTime;
-            _fillImage.color =
-                _gameManager.productionGradientColor.Evaluate(_waitingSlider.value / _waitingSlider.maxValue);
-        }
+        if (_productionButton.interactable) return;
+        _waitingSlider.value -= Time.deltaTime;
+        _fillImage.color =
+            _gameManager.productionGradientColor.Evaluate(_waitingSlider.value / _waitingSlider.maxValue);
     }
 
+    // ReSharper disable Unity.PerformanceAnalysis
     private IEnumerator AddResource()
     {
-        yield return new WaitForSeconds(BaseProductionTime);
-        ResourceBank.ChangeResource(currentGameResource, 1);
+        yield return new WaitForSeconds(_totalProductionTime);
+        ResourceBank.ChangeResource(currentGameResource, _productAmount);
         _productionButton.interactable = true;
         _waitingSlider.gameObject.SetActive(false);
     }
