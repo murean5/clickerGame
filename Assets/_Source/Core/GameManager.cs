fd38bffd43ee1c6a1d78ddd22ee75c5a2aa47a44
Anton Muravev <ayumuravev@edu.hse.ru>
2023-10-11 17:11:54 +0300

    Sliders
    feat!: now it is possible to see beautiful sliders of production progress

--- Assets/_Source/Core/GameManager.cs
+++ Assets/_Source/Core/GameManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public Gradient productionGradientColor;
     private void Awake()
     {
         ResourceBank.ChangeResource(GameResource.Humans, 10);
--- Assets/_Source/Core/ProductionBuilding.cs
+++ Assets/_Source/Core/ProductionBuilding.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Core;
@@ -7,15 +8,44 @@ using UnityEngine.UI;
 public class ProductionBuilding : MonoBehaviour
 {
     [SerializeField] private GameResource currentGameResource;
-    private const float ProductionTime = 2f;
+    private const float BaseProductionTime = 1.1f;
+    private Button _productionButton;
+    private GameManager _gameManager;
+    private Image _fillImage;
+    private Slider _waitingSlider;
 
     private void Awake()
     {
-        GetComponent<Button>().onClick.AddListener(OnButtonPushed);
+        if (Camera.main != null) _gameManager = Camera.main.GetComponent<GameManager>();
+        _productionButton = GetComponent<Button>();
+        _productionButton.onClick.AddListener(Product);
+        _waitingSlider = _productionButton.transform.Find("WaitingSlider").GetComponent<Slider>();
+        _fillImage = _waitingSlider.transform.Find("Fill Area/Fill").GetComponent<Image>();
     }
 
-    private void OnButtonPushed()
+    private void Product()
     {
+        _waitingSlider.value = _waitingSlider.maxValue;
+        _productionButton.interactable = false;
+        _waitingSlider.gameObject.SetActive(true);
+        StartCoroutine(AddResource());
+    }
+
+    private void Update()
+    {
+        if (!_productionButton.interactable)
+        {
+            _waitingSlider.value -= Time.deltaTime;
+            _fillImage.color =
+                _gameManager.productionGradientColor.Evaluate(_waitingSlider.value / _waitingSlider.maxValue);
+        }
+    }
+
+    private IEnumerator AddResource()
+    {
+        yield return new WaitForSeconds(BaseProductionTime);
         ResourceBank.ChangeResource(currentGameResource, 1);
+        _productionButton.interactable = true;
+        _waitingSlider.gameObject.SetActive(false);
     }
 }
